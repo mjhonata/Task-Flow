43026e5739623e9f0c10b83783f6cddc42342cb9
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-11-24 20:00:12 -0300

    remove class1 and add class base

---
+++ TaskFlow/src/TaskFlow.Domain/Entities/Entity.cs
@@ -0,0 +1,12 @@
+namespace TaskFlow.Domain.Entities;
+
+public abstract class Entity : IEquatable<Entity>
+{
+    public Guid Id { get; private set; }
+
+    protected Entity()
+    {
+        //Sempre gera um novo Guid ao criar a entidade
+        Id = Guid.NewGuid();
+    }
+}
