0fc77bf5d08776b4538f1ca3b85b065b4014e89c
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-11-24 23:21:22 -0300

    add regions

--- TaskFlow/src/TaskFlow.Domain/Entities/TodoItem.cs
+++ TaskFlow/src/TaskFlow.Domain/Entities/TodoItem.cs
@@ -2,15 +2,35 @@ namespace TaskFlow.Domain.Entities;
 
 public class TodoItem : Entity
 {
+    #region Properties
     public string Title { get; private set; }
     public bool Done { get; private set; }
     public DateTime Date { get; private set; }
     public string User { get; private set; }
+    #endregion
+    #region Constructors
     public TodoItem(string title, bool done, DateTime date, string user)
     {
         Title = title;
-        Done = done;
+        Done = false;
         Date = date;
         User = user;
     }
+    #endregion
+    #region Methods
+    //Marca a tarefa como concluída
+    public void MarkAsDone()
+    {
+        Done = true;
+    }
+    //Marca a tarefa como não concluída
+    public void MarkAsUndone()
+    {
+        Done = false;
+    }
+    public void UpdateTitle(string title)
+    {
+        Title = title;
+    }
+    #endregion
 }
