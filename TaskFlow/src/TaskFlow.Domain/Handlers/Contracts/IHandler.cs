15c2db7873cb5ffca653de43eb01c2e7aaa6e7d4
Matheus Jhonatas dos Santos <129222113+MatheusJhonatas@users.noreply.github.com>
2025-12-01 21:27:07 -0300

    add coments

--- TaskFlow/src/TaskFlow.Domain/Handlers/Contracts/IHandler.cs
+++ TaskFlow/src/TaskFlow.Domain/Handlers/Contracts/IHandler.cs
@@ -1,7 +1,7 @@
 using TaskFlow.Domain.Commands.Contracts;
 
 namespace TaskFlow.Domain.Handlers.Contracts;
-
+//where dois pontos é para restringir o tipo generico T, desde que t seja um comando
 public interface IHandler<T> where T : ICommand
 {
     ICommandResult Handle(T command);
