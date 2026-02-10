9499f7d5e2d535101d0271b865f9dc1f2cdf63e2
mjhonata <matheus.jhonatassantos@emeal.nttdata.com>
2026-02-10 09:41:32 -0300

    develop

--- TaskFlow/tests/Domain.Tests/Repositories/FakeTodoRepository.cs
+++ TaskFlow/tests/Domain.Tests/Repositories/FakeTodoRepository.cs
@@ -3,6 +3,7 @@
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 using TaskFlow.Domain.Entities;
 using TaskFlow.Domain.Repositories;
 namespace TaskFlow.Tests.Domain.Tests.Repositories;
@@ -37,6 +38,8 @@ public class FakeTodoRepository : ITodoRepository
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
+=======
+>>>>>>> Stashed changes
     using System;
     using System.Collections.Generic;
     using System.Linq;
@@ -52,6 +55,9 @@ namespace Domain.Tests.Repositories;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
+>>>>>>> Stashed changes
+=======
 >>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
